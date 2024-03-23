50db2c82bbbc7124c13c6274fb5a338daaff6a58
Goutham Kumar S <113292942+GouthamKumar025@users.noreply.github.com>
2024-03-23 05:50:27 +0530

    Update knn.py

--- knn.py
+++ knn.py
@@ -9,3 +9,23 @@ knn = KNeighborsClassifier(n_neighbors=6)
 
 # Fit the classifier to the data
 knn.fit(X,y)
+
+#calculating the accuracy scores
+# Create neighbors
+from sklearn.neighbors import KNeighborsClassifier
+neighbors = np.arange(1, 12)
+train_accuracies = {}
+test_accuracies = {}
+
+for neighbor in neighbors:
+  
+	# Set up a KNN Classifier
+	knn = KNeighborsClassifier(n_neighbors=neighbor)
+  
+	# Fit the model
+	knn.fit(X_train, y_train)
+  
+	# Compute accuracy
+	train_accuracies[neighbor] = knn.score(X_train, y_train)
+	test_accuracies[neighbor] = knn.score(X_test, y_test)
+print(neighbors, '\n', train_accuracies, '\n', test_accuracies)
